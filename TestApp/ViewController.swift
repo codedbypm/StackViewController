962ec39fb86df7f67b91a98b92a17a0137a76a04
Paolo Moroni <paolo@codedby.pm>
2019-04-01 16:31:19 +0200

    Add TestApp to project

---
+++ TestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by Paolo Moroni on 01/04/2019.
+//  Copyright © 2019 codedby.pm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

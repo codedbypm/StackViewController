a847966b2f4407488e7120919d24e265f5e9069d
Paolo Moroni <paolo@codedby.pm>
2019-04-01 16:13:52 +0200

    Create XCode project

---
+++ NavigationController/NavigationController.h
@@ -0,0 +1,19 @@
+//
+//  NavigationController.h
+//  NavigationController
+//
+//  Created by Paolo Moroni on 01/04/2019.
+//  Copyright © 2019 codedby.pm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NavigationController.
+FOUNDATION_EXPORT double NavigationControllerVersionNumber;
+
+//! Project version string for NavigationController.
+FOUNDATION_EXPORT const unsigned char NavigationControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NavigationController/PublicHeader.h>
+
+

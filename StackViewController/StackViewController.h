c86c4c96bf2551fa90b71fde87596747dbad8c8b
Paolo Moroni <paolo@codedby.pm>
2019-04-02 23:10:37 +0200

    Rename everything else into StackViewController

---
+++ StackViewController/StackViewController.h
@@ -0,0 +1,15 @@
+//
+//  StackViewController.h
+//  StackViewController
+//
+//  Created by Paolo Moroni on 01/04/2019.
+//  Copyright © 2019 codedby.pm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StackViewController.
+FOUNDATION_EXPORT double StackViewControllerVersionNumber;
+
+//! Project version string for StackViewController.
+FOUNDATION_EXPORT const unsigned char StackViewControllerVersionString[];

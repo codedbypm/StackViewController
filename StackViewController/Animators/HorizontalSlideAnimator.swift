d64ac3e3336bc4b76d04578d58e6a133453e9e23
Paolo Moroni <paolo@codedby.pm>
2019-04-10 19:05:03 +0200

    Correct a comment name

--- StackViewController/Animators/HorizontalSlideAnimator.swift
+++ StackViewController/Animators/HorizontalSlideAnimator.swift
@@ -1,6 +1,6 @@
 //
 //  HorizontalSlideAnimator.swift
-//  NavigationController
+//  StackViewController
 //
 //  Created by Paolo Moroni on 01/04/2019.
 //  Copyright © 2019 codedby.pm. All rights reserved.

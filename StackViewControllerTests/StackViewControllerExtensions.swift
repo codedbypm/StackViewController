49c66641e94dc386c9d0714bece9073e49ee8e55
Paolo Moroni <paolo@codedby.pm>
2019-04-24 20:26:23 +0200

    Add StackViewController extension to test target

---
+++ StackViewControllerTests/StackViewControllerExtensions.swift
@@ -0,0 +1,9 @@
+//
+//  StackViewControllerExtensions.swift
+//  StackViewControllerTests
+//
+//  Created by Paolo Moroni on 24/04/2019.
+//  Copyright © 2019 codedby.pm. All rights reserved.
+//
+
+import Foundation

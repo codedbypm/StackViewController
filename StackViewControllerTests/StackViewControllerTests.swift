aa2c3dfbb367091de01101e1c32301d851d24abb
Paolo Moroni <paolo@codedby.pm>
2019-04-24 20:27:21 +0200

    Add test for topViewController and visibleViewController

--- StackViewControllerTests/StackViewControllerTests.swift
+++ StackViewControllerTests/StackViewControllerTests.swift
@@ -19,12 +19,105 @@ class StackViewControllerTests: XCTestCase {
     }
 
     override func tearDown() {
+        sut = nil
         super.tearDown()
+    }
+
+    // MARK: - topViewController
+
+    func testThat_whenTheStackIsEmpty_topViewControllerIsNil() {
+        // Arrange
+        sut = StackViewController.withEmptyStack()
+
+        // Act
+        let topViewController = sut.topViewController
+
+        // Assert
+        XCTAssertNil(topViewController)
+    }
+
+    func testThat_whenTheStackIsNotEmpty_topViewControllerEqualsTheLastElementOfTheStack() {
+        // Arrange
+        sut = StackViewController.withNumberOfViewControllers(3)
+
+        // Act
+        let topViewController = sut.topViewController
+
+        // Assert
+        XCTAssertNotNil(topViewController)
+        XCTAssertEqual(topViewController, sut.viewControllers.last)
+    }
+
+    // MARK: - visibleViewController
+
+    func testThat_whenTheStackIsEmpty_visibleViewControllerIsNil() {
+        // Arrange
+        sut = StackViewController.withEmptyStack()
+
+        // Act
+        let visibleViewController = sut.visibleViewController
+
+        // Assert
+        XCTAssertNil(visibleViewController)
+    }
+
+    func testThat_whenTopViewControllerViewIsNotLoaded_visibleViewControllerIsNil() {
+        // Arrange
+        sut = StackViewController.withNumberOfViewControllers(3)
+        guard let topViewController = sut.topViewController, !topViewController.isViewLoaded else {
+            XCTFail("topViewController's view should not be loaded")
+            return
+        }
+
+        // Act
+        let visibleViewController = sut.visibleViewController
+
+        // Assert
+        XCTAssertNil(visibleViewController)
+    }
+
+    func testThat_whenTopViewControllerViewIsLoadedButNotAddedToTheViewHierarchy_visibleViewControllerIsNil() {
+        // Arrange
+        sut = StackViewController
+            .withNumberOfViewControllers(3)
+            .loadingTopViewControllerView()
+
+        guard let topViewController = sut.topViewController, topViewController.isViewLoaded else {
+            XCTFail("topViewController's view should be loaded")
+            return
+        }
+
+        XCTAssertNil(topViewController.view.superview)
+
+        // Act
+        let visibleViewController = sut.visibleViewController
+
+        // Assert
+        XCTAssertNil(visibleViewController)
+    }
+
+    func testThat_whenTopViewControllerViewIsAddedToTheStackViewControllerView_visibleViewControllerIsNotNil() {
+        // Arrange
+        sut = StackViewController
+            .withNumberOfViewControllers(3)
+            .showingTopViewControllerView()
+
+        guard let topViewController = sut.topViewController, topViewController.view.superview != nil else {
+            XCTFail("topViewController's view should have a superview")
+            return
+        }
+
+        // Act
+        let visibleViewController = sut.visibleViewController
+
+        // Assert
+        XCTAssertNotNil(visibleViewController)
+        XCTAssertEqual(visibleViewController, topViewController)
     }
 
     // MARK: - pushViewController(_: animated:)
 
-    func testThat_whenPushingViewControllerWhichIsAlreadyOnTheStack_theViewControllerIsNotPushed() {
+    func testThat_whenPushingViewControllerWhichIsAlreadyOnTheStack_theViewControllerIsNotAddedToTheStack() {
         // Arrange
         let testViewController = UIViewController()
         sut = StackViewController(viewControllers: [testViewController])

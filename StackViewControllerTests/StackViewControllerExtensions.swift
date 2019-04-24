c691537e171f4b49e4a93c0620363b06ab6bf29b
Paolo Moroni <paolo@codedby.pm>
2019-04-24 20:27:03 +0200

    Create some handy methods for unit test

--- StackViewControllerTests/StackViewControllerExtensions.swift
+++ StackViewControllerTests/StackViewControllerExtensions.swift
@@ -7,3 +7,35 @@
 //
 
 import Foundation
+@testable import StackViewController
+
+extension StackViewController {
+
+    static var dummy: StackViewController {
+        return StackViewController(viewControllers: [])
+    }
+
+    static func withEmptyStack() -> StackViewController {
+        return .withNumberOfViewControllers(0)
+    }
+
+    static func withNumberOfViewControllers(_ count: UInt) -> StackViewController {
+        let stack = (0..<count).map { _ in return UIViewController() }
+        return StackViewController(viewControllers: stack)
+    }
+}
+
+
+extension StackViewController {
+
+    func loadingTopViewControllerView() -> StackViewController {
+        _ = topViewController?.view
+        return self
+    }
+
+    func showingTopViewControllerView() -> StackViewController {
+        guard let topViewControllerView = topViewController?.view else { return .dummy }
+        view.addSubview(topViewControllerView)
+        return self
+    }
+}

0aa1bbf2798b9406e13502028077ee87252d7e47
Paolo Moroni <paolo@codedby.pm>
2019-05-03 20:28:15 +0200

    Add files from Swift 5.1 diffing feature

---
+++ StackViewController/Swift 5 diffing/Collection+Extensions.swift
@@ -0,0 +1,37 @@
+//
+//  Collection+Extensions.swift
+//  StackViewController
+//
+//  Created by Paolo Moroni on 03/05/2019.
+//  Copyright © 2019 codedby.pm. All rights reserved.
+//
+
+import Foundation
+
+extension Collection {
+    // Utility method for collections that wish to implement
+    // CustomStringConvertible and CustomDebugStringConvertible using a bracketed
+    // list of elements, like an array.
+    internal func _makeCollectionDescription(
+        withTypeName type: String? = nil
+        ) -> String {
+        var result = ""
+        if let type = type {
+            result += "\(type)(["
+        } else {
+            result += "["
+        }
+
+        var first = true
+        for item in self {
+            if first {
+                first = false
+            } else {
+                result += ", "
+            }
+            debugPrint(item, terminator: "", to: &result)
+        }
+        result += type != nil ? "])" : "]"
+        return result
+    }
+}

1865246740254f88eadb9658b08c252ef6900cfa
Paolo Moroni <paolo@codedby.pm>
2019-05-06 17:47:07 +0200

    Update Transition struct with animated and interactive properties

--- StackViewController/Transition.swift
+++ StackViewController/Transition.swift
@@ -12,4 +12,19 @@ struct Transition {
     let operation: StackViewController.Operation
     let from: UIViewController?
     let to: UIViewController?
+    let animated: Bool
+    let interactive: Bool
+
+    init(operation: StackViewController.Operation,
+         from: UIViewController?,
+         to: UIViewController?,
+         animated: Bool = true,
+         interactive: Bool = false) {
+
+        self.operation = operation
+        self.from = from
+        self.to = to
+        self.animated = animated
+        self.interactive = interactive
+    }
 }

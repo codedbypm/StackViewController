8ebc372fca362474304d72b4b54978c1e2718229
Paolo Moroni <paolo@codedby.pm>
2019-05-08 23:34:26 +0200

    Add tests for push and pushStack

--- StackViewControllerTests/StackInteractorTests.swift
+++ StackViewControllerTests/StackInteractorTests.swift
@@ -21,24 +21,121 @@ class StackInteractorTests: XCTestCase {
         super.tearDown()
     }
 
+    // MARK: - Inner helper class
 
-     // MARK: - init(stack:)
+    class InteractorDelegate: StackInteractorDelegate {
+        var change: CollectionDifference<UIViewController>?
+        var didCallStackDidChange = false
+        func stackDidChange(_ change: CollectionDifference<Stack.Element>) {
+            didCallStackDidChange = true
+            self.change = change
+        }
+    }
 
-    func testThat_whenPassingAStackContainingDuplicates_initThrowsErrorDuplicateViewControllers() {
+    // MARK: - push(_ : UIViewController, animated: Bool)
+
+    func testThat_whenPushingAViewController_thisIsAppendedToTheCurrentStack() {
         // Arrange
-        let viewController = UIViewController()
-        let stack = [viewController, viewController]
+        let currentStack = sut.stack
+        let pushedViewController = UIViewController()
 
         // Act
-        sut = StackInteractor(stack: stack)
+        sut.push(pushedViewController, animated: true)
+
+        // Assert
+        XCTAssertEqual(sut.stack, currentStack + [pushedViewController])
+    }
+
+    func testThat_whenPushingAViewController_theStackDidChangeMethodIsInvokedOnTheDelegateWithTheCollectionDifferenceAsInput() {
+        // Arrange
+        let interactorDelegate = InteractorDelegate()
+        sut.delegate = interactorDelegate
+
+        let currentStack = sut.stack
+        let pushedViewController = UIViewController()
+        let insertions: [CollectionDifference<Stack.Element>.Change] = [
+            .insert(offset: currentStack.endIndex, element: pushedViewController, associatedWith: nil)
+        ]
+        let expectedDifference = CollectionDifference<UIViewController>.init(insertions)
+
+        // Act
+        sut.push(pushedViewController, animated: true)
 
         // Assert
 
-        XCTAssertThrowsError(
-            sut = StackInteractor(stack: stack),
-            "StackInteractor init should throw when passing a stack containing duplicate elements as its input"
-        ) { error in
-            XCTAssertEqual(error as? StackViewControllerError, StackViewControllerError.duplicateViewControllers)
-        }
+        XCTAssertTrue(sut.delegate === interactorDelegate)
+        XCTAssertTrue(interactorDelegate.didCallStackDidChange)
+        XCTAssertEqual(interactorDelegate.change, expectedDifference)
     }
+
+    func testThat_whenPushingAViewControllerAlreadyPresentInTheStack_theCurrentStackIsNotChanged() {
+        // Arrange
+        let currentStack = sut.stack
+        let pushedViewController = StackViewController.knwownViewControllerA
+
+        // Act
+        sut.push(pushedViewController, animated: true)
+
+        // Assert
+        XCTAssertEqual(sut.stack, currentStack)
+    }
+
+    // MARK: - push(_: Stack, animated: Bool)
+
+    func testThat_whenPushingAValidStack_thisIsAppendedToTheCurrentStack() {
+        // Arrange
+        let currentStack = sut.stack
+        let pushedStack = [UIViewController()]
+
+        // Act
+        sut.push(pushedStack, animated: true)
+
+        // Assert
+        XCTAssertEqual(sut.stack, currentStack + pushedStack)
+    }
+
+    func testThat_whenPushingAValidStack_theStackDidChangeMethodIsInvokedOnTheDelegateWithTheCollectionDifferenceAsInput() {
+        // Arrange
+        let interactorDelegate = InteractorDelegate()
+        sut.delegate = interactorDelegate
+
+        let currentStack = sut.stack
+        let pushedViewController = UIViewController()
+        let insertions: [CollectionDifference<Stack.Element>.Change] = [
+            .insert(offset: currentStack.endIndex, element: pushedViewController, associatedWith: nil)
+        ]
+        let expectedDifference = CollectionDifference<UIViewController>.init(insertions)
+
+        // Act
+        sut.push([pushedViewController], animated: true)
+
+        // Assert
+
+        XCTAssertTrue(sut.delegate === interactorDelegate)
+        XCTAssertTrue(interactorDelegate.didCallStackDidChange)
+        XCTAssertEqual(interactorDelegate.change, expectedDifference)
+    }
+
+    func testThat_whenPushingAStackResultingInANewStackWithDuplicates_theCurrentStackIsNotChanged() {
+        // Arrange
+        let currentStack = sut.stack
+        let pushedStack = [StackViewController.knwownViewControllerA]
+
+        // Act
+        sut.push(pushedStack, animated: true)
+
+        // Assert
+        XCTAssertEqual(sut.stack, currentStack)
+    }
+}
+
+
+extension StackInteractorDelegate {
+    func didAddStackElements(_: Stack) {}
+    func didRemoveStackElements(_: Stack) {}
+    func didReplaceStack(_ oldStack: Stack, with newStack: Stack) {}
+
+    func stackDidChange(_ change: CollectionDifference<Stack.Element>) {}
+    func didCreateTransition(_: Transition) {}
+
 }

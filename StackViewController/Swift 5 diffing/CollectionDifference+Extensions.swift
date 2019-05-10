1700296f7520a4157a2df1815219d22f87d426f2
Paolo Moroni <paolo@codedby.pm>
2019-05-10 09:18:05 +0200

    Add extension to CollectionDifference

---
+++ StackViewController/Swift 5 diffing/CollectionDifference+Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  CollectionDifference+Extensions.swift
+//  StackViewController
+//
+//  Created by Paolo Moroni on 10/05/2019.
+//  Copyright © 2019 codedby.pm. All rights reserved.
+//
+
+import Foundation
+
+extension CollectionDifference {
+
+    static var noDifference: CollectionDifference {
+        return CollectionDifference([])!
+    }
+}

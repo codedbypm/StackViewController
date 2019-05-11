7ce8553cbd0c3eafdba169ad61c9027f1ccc1aa2
Paolo Moroni <paolo@codedby.pm>
2019-05-11 19:47:08 +0200

    Add inverted property to CollectionDifference and Change

--- StackViewController/Swift 5 diffing/CollectionDifference+Extensions.swift
+++ StackViewController/Swift 5 diffing/CollectionDifference+Extensions.swift
@@ -13,4 +13,20 @@ extension CollectionDifference {
     static var noDifference: CollectionDifference {
         return CollectionDifference([])!
     }
+
+    var inverted: CollectionDifference {
+        return CollectionDifference(map { $0.inverted })!
+    }
+}
+
+extension CollectionDifference.Change {
+
+    var inverted: CollectionDifference.Change {
+        switch self {
+        case .insert(offset: let o, element: let e, associatedWith: let a):
+            return .remove(offset: o, element: e, associatedWith: a)
+        case .remove(offset: let o, element: let e, associatedWith: let a):
+            return .insert(offset: o, element: e, associatedWith: a)
+        }
+    }
 }
